ec3b13d6e05c8544ff84476733d7f5d36f457a0e
MARSHAL327 <sanya.shvedenko@mail.ru>
2023-07-14 01:23:37 +0300

    Add button for hide sidebar. Update logic for show filter.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,23 +3,38 @@ import Filter from "../components/Filter/components/Filter";
 import {observer} from "mobx-react-lite";
 import FilterStore from "../components/Filter/store/filter.store";
 import {Transition} from "@headlessui/react";
+import cc from "classcat";
+import {ReactComponent as Chevron} from "../assets/icons/Chevron.svg";
+import {useEffect, useState} from "react";
+import Ripple from "../components/RedefinedTags/Ripple/Ripple";
 
 const HomePage = observer(({tabItems}) => {
+    let [hideSidebar, setHideSidebar] = useState(false)
+    let filterStyles = {
+        transform: !FilterStore.isOpen ? "translateX(0)" : "translateX(calc(100% - 21px))",
+    }
+
     return (
         <>
-            <div className={"flex drop-shadow-xl h-full transition absolute top-0 left-0"}>
+            <div className={cc(["flex drop-shadow-xl h-full transition absolute top-0 left-0 z-20", {
+                "translate-x-0": !FilterStore.isOpen && !hideSidebar,
+                "-translate-x-full": hideSidebar,
+                "-translate-x-[200%]": FilterStore.isOpen && hideSidebar
+            }])}>
                 <Sidebar tabItems={tabItems}/>
-                <Transition
-                    show={FilterStore.isOpen}
-                    enter="transition duration-75"
-                    enterFrom="-translate-x-full"
-                    enterTo="translate-x-0"
-                    leave="transition duration-150"
-                    leaveFrom="translate-x-0"
-                    leaveTo="-translate-x-full"
-                >
+                <div style={filterStyles} className={cc(["transition flex absolute h-full"])}>
                     <Filter/>
-                </Transition>
+                    <div
+                        className={cc(["overflow-hidden rotate-0 relative left-5 top-7 bg-white hover:bg-gray-200 " +
+                        "hover:cursor-pointer transition rounded-xl py-4 px-3 h-fit", {
+                            "rotate-180": hideSidebar
+                        }])}
+                        onClick={() => setHideSidebar(hideSidebar => !hideSidebar)}
+                    >
+                        <Chevron/>
+                        <Ripple color={"rgba(0,0,0,.6)"}/>
+                    </div>
+                </div>
             </div>
             {/*<AdminBtn/>*/}
         </>
