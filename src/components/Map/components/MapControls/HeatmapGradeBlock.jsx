8460afd985560755a0a61912f8df09c1e416c19d
MARSHAL327 <sanya.shvedenko@mail.ru>
2023-10-04 00:10:33 +0300

    Fixed HeatmapGradeBlock.jsx position

--- src/components/Map/components/MapControls/HeatmapGradeBlock.jsx
+++ src/components/Map/components/MapControls/HeatmapGradeBlock.jsx
@@ -9,30 +9,31 @@ const HeatmapGradeBlock = observer(() => {
     let jetGradient = "#000080, #0000FF, #0054FF, #00ADFF, #19FFDE, #60FF97, #A7FF50, #EEFF09, #FFAE00, #FF6000, #FA0F00, #960000"
 
     return (
-        <div className={cc([MapStore.blurBackgroundClasses, "!p-0 !relative !rounded-full w-fit"])}>
-            <div className={"flex gap-3 items-center relative"}>
-                <div className={"flex justify-between gap-4 text-white rounded-full py-1 px-5 min-w-[300px]"}
-                     style={{
-                         background: `linear-gradient(90deg, ${jetGradient})`
-                     }}
-                >
-                    {MapStore.selectedAdditionalLayer.gradeRange.map((rangeValue, idx) =>
-                        <div style={{
-                            textShadow: "0px 0px 7px rgba(0, 0, 0, 1)"
-                        }} className={""} key={idx}>
-                            {rangeValue}
-                        </div>
-                    )}
+        <div className={cc([
+            MapStore.blurBackgroundClasses,
+            "!p-0 relative !rounded-full w-fit flex gap-3 items-center lg:absolute lg:bottom-16 lg:right-3"
+        ])}>
+            <div className={"flex justify-between gap-4 text-white rounded-full py-1 px-5 min-w-[260px]"}
+                 style={{
+                     background: `linear-gradient(90deg, ${jetGradient})`
+                 }}
+            >
+                {MapStore.selectedAdditionalLayer.gradeRange.map((rangeValue, idx) =>
+                    <div style={{
+                        textShadow: "0px 0px 7px rgba(0, 0, 0, 1)"
+                    }} className={""} key={idx}>
+                        {rangeValue}
+                    </div>
+                )}
 
-                </div>
-                <div>
-                    {
-                        indicationData.units &&
-                        <div className={"font-bold mr-3"}>
-                            {indicationData.unitsFull || indicationData.units}
-                        </div>
-                    }
-                </div>
+            </div>
+            <div>
+                {
+                    indicationData.units &&
+                    <div className={"font-bold mr-3"}>
+                        {indicationData.unitsFull || indicationData.units}
+                    </div>
+                }
             </div>
         </div>
     )
